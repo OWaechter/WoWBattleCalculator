aae224f12de8ae563a565406bd9b369779cbb926
Oliver Wächter <o.waechter@gmx.net>
2022-08-31 23:44:32 +0200

    Initial Commit

---
+++ WoWBattleCalculator/WoWBattleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WoWBattleCalculatorApp.swift
+//  WoWBattleCalculator
+//
+//  Created by Oliver Wächter on 31.08.22.
+//
+
+import SwiftUI
+
+@main
+struct WoWBattleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
